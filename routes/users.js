af9f14830c09b2d886cd001025921f4d4bb435ad
S542301 <77767471+S542301@users.noreply.github.com>
2021-03-24 21:29:09 -0500

    user file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var swa=0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  swa=swa+1;
+  res.send('User accesses are: '+swa);
 });
 
 module.exports = router;
